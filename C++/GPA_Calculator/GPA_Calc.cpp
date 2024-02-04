a2251538ecaf2ced755033d24211e5a096c70fa4
BlitzMK <sec180005@utdallas.edu>
2024-02-03 22:06:02 -0600

    fun little GPA calc

--- C++/GPA_Calculator/GPA_Calc.cpp
+++ C++/GPA_Calculator/GPA_Calc.cpp
@@ -1,15 +1,65 @@
 //promps user to add classes until they select calculate and a 
-
+#include <vector>
+#include <iostream>
+#include <string>
 using namespace std;
+
+class Course{
+    public:
+        double GPA;
+        string name; 
+        int credit_hours;
+
+        Course(){
+            GPA = 0;
+            name = "None";
+            credit_hours = 0;
+        }
+
+        Course(string b, double a,  int c){
+            GPA = a;
+            name = b;
+            credit_hours = c;
+        }
+
+        void print(){
+            cout << name << " " << to_string(credit_hours) << " " << to_string(GPA) << endl;
+        }
+
+        friend istream& operator>>(istream &is, Course &C){
+            is >> C.name >> C.credit_hours >> C.GPA ;
+            return is;
+        }
+
+};
 
 int main(){
 
-}
-
-void Add_Course(){
-
-}
-
-double calculate_GPA(){
     
+    int class_number;
+    double GPA_total = 0;
+    int hour_total = 0;
+
+    cout << "Please Enter the Number of Classes you have taken: ";
+    cin >> class_number;
+    vector <Course> classes(class_number);
+
+    for (int i = 0; i < class_number; i++)
+    {
+        cout << "please enter a class in the following format, name credit_hours GPA" << endl;
+        cin >> classes[i];
+        GPA_total += classes[i].GPA * classes[i].credit_hours;
+        hour_total += classes[i].credit_hours;
+    }
+
+    cout << endl << "Classes:" << endl;
+
+    for(auto &ele : classes){
+        ele.print();
+    }
+
+    cout << endl << "Total Hours: " << hour_total << endl;
+    cout << "Cumulitive GPA: " << GPA_total/hour_total << endl;
+
+    return 0;
 }
